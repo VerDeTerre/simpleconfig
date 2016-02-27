f579421654dd999de70369ff7433e851c2ee5113
jtbnntt <10692730+jtbnntt@users.noreply.github.com>
2016-02-27 11:27:25 -0800

    Remove duplicate lines in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup
 setup(
     name='simpleconfig',
     version='0.0.0',
-    name='simpleconfig',
-    version='0.0.0',
     author='John Tye Bennett',
     author_email='john.tye.bennett@gmail.com',
     description='A tool for loading configurations for multiple environments',
